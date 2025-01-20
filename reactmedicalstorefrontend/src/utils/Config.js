8a1303cc1198c0bb140f97d7b5966254f6841c0b
Maunducyrus <maunducyrus123@gmail.com>
2025-01-20 15:29:58 +0300

    Debugging

--- reactmedicalstorefrontend/src/utils/Config.js
+++ reactmedicalstorefrontend/src/utils/Config.js
@@ -1,4 +1,4 @@
-class Config{
+class Config {
     static loginUrl="http://127.0.0.1:8000/api/gettoken/";
     static homeUrl="/home";
 
@@ -7,7 +7,7 @@ class Config{
         {"index":0, title:"Home", url:"/home", icons: "home"},
         {"index":1, title:"Company", url:"/company", icons: "assessment"},
 
-    ]
+    ];
 }
 
 export default Config; 
