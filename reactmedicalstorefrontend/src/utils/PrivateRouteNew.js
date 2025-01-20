bd3caffd72dc999e85ac8603730cdf13bccb3736
Maunducyrus <maunducyrus123@gmail.com>
2025-01-20 16:50:29 +0300

    it worked

--- reactmedicalstorefrontend/src/utils/PrivateRouteNew.js
+++ reactmedicalstorefrontend/src/utils/PrivateRouteNew.js
@@ -1,22 +1,37 @@
-import React, { Component } from "react";
-import { Route, Navigate, Router, Routes } from "react-router-dom"
-import AuthHandler from "./AuthHandler";
-import MainComponent from "../components/MainComponent"
+// import React, { Component } from "react";
+// import { Route, Navigate } from "react-router-dom"
+// import AuthHandler from "./AuthHandler";
+// import MainComponent from "../components/MainComponent"
 
-export var PrivateRouteNew = ({ page, activepage, ...rest }) => {
-    return (
-        <Route
-            {...rest}
-           render={(props) => 
-            AuthHandler.loggedIn() ? (
-                <MainComponent Page={Page} activepage={activepage} {...props}/>
-            ) : (
-                <Navigate to="/" />
-            )
-          }
-        />
+// export var PrivateRouteNew = ({ page, activepage, ...rest }) => {
+//     return (
+//         <Route
+//             {...rest}
+//            render={(props) => 
+//             AuthHandler.loggedIn() ? (
+//                 <MainComponent Page={page} activepage={activepage} {...props}/>
+//             ) : (
+//                 <Navigate to="/" />
+//             )
+//           }
+//         />
     
-    );
+//     );
+// };
+
+// export default PrivateRouteNew;
+
+import React from "react";
+import { Navigate } from "react-router-dom";
+import AuthHandler from "./AuthHandler";
+import MainComponent from "../components/MainComponent";
+
+export const PrivateRouteNew = ({ page, activepage }) => {
+  if (AuthHandler.loggedIn()) {
+    return <MainComponent Page={page} activepage={activepage} />;
+  } else {
+    return <Navigate to="/" />;
+  }
 };
 
 export default PrivateRouteNew;
