04240142dfb69cb666977e473b458dd91877bd19
Maunducyrus <maunducyrus123@gmail.com>
2025-01-20 16:04:40 +0300

    PrivateRouteNew Debugging

--- reactmedicalstorefrontend/src/utils/PrivateRouteNew.js
+++ reactmedicalstorefrontend/src/utils/PrivateRouteNew.js
@@ -1,54 +1,23 @@
-import React from "react";
+import React, { Component } from "react";
 import { Route, Navigate, Router, Routes } from "react-router-dom"
 import AuthHandler from "./AuthHandler";
 import MainComponent from "../components/MainComponent"
 
-// export var PrivateRouteNew = ({ page, ...rest }) =>{
-//     return (
-//     <Route
-//     {...rest}
-//     element={() => 
-//         AuthHandler.loggedIn() ? (<MainComponent page={page} activepage={activepage} />) : (<Navigate to='/' />)
-//     } 
-//     />
-//     );
-// }
 export var PrivateRouteNew = ({ page, activepage, ...rest }) => {
     return (
-        <Router>
-       <Routes>
         <Route
             {...rest}
-            element={
-                AuthHandler.loggedIn() 
-                    ? <MainComponent page={page} activepage={activepage} />
-                    : <Navigate to="/" />
-            }
+           render={(props) => 
+            AuthHandler.loggedIn() ? (
+                <MainComponent Page={Page} activepage={activepage} {...props}/>
+            ) : (
+                <Navigate to="/" />
+            )
+          }
         />
-    </Routes>
-    </Router>
-       
+    
     );
 };
 
 export default PrivateRouteNew;
 
-// import React from "react";
-// import { Route, Navigate } from "react-router-dom";
-// import AuthHandler from "./AuthHandler";
-// import MainComponent from "../components/MainComponent";
-
-// export var PrivateRouteNew = ({ page, activepage, ...rest }) => {
-//     return (
-//         <Route
-//             {...rest}
-//             element={
-//                 AuthHandler.loggedIn() 
-//                     ? <MainComponent page={page} activepage={activepage} />
-//                     : <Navigate to="/" />
-//             }
-//         />
-//     );
-// };
-
-// export default PrivateRouteNew;
