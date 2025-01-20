3cbf04cf984a76ed8ea0ac5704cb87aef3369c22
Maunducyrus <maunducyrus123@gmail.com>
2025-01-20 15:47:28 +0300

    Removed Router and Routes in PrivateRoute

--- reactmedicalstorefrontend/src/utils/PrivateRoute.js
+++ reactmedicalstorefrontend/src/utils/PrivateRoute.js
@@ -1,39 +1,21 @@
-import React from "react";
+import React, {Component} from "react";
 import { Route, Router, Routes, Navigate } from "react-router-dom"
 import AuthHandler from "./AuthHandler";
 
 export var PrivateRoute = ({component:Component,...rest}) =>{
     console.log({ ...rest });
     console.log(rest);
-    return (
 
-        <Router>
-            <Routes>
-    <Route
-    {...rest}
-    // used element instead of render in Props
-    element={(props) => 
-        AuthHandler.loggedIn() ? (<Component {...props}/>) : <Navigate to="/" />} 
+    return (
+      <Route
+            {...rest}
+            // used element instead of render in Props
+             element={(props) => 
+                  AuthHandler.loggedIn() ? (<Component {...props}/>) : <Navigate to="/" />} 
         />
-        </Routes>
-        </Router>
+        
     );
 }
 
 
-// export var PrivateRoute = ({ component: Component, ...rest }) => {
-//     console.log({ ...rest });
-//     console.log(rest);
-//     return (
-//         <Route
-//             {...rest}
-//             element={
-//                 AuthHandler.loggedIn() 
-//                     ? <Component {...rest} /> 
-//                     : <Navigate to="/" />
-//             }
-//         />
-//     );
-// };
 
-// export default PrivateRoute;
