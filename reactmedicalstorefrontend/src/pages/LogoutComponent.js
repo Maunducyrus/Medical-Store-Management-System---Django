94b45fae607a0203b1378acbe64351ea31930081
Maunducyrus <maunducyrus123@gmail.com>
2025-01-23 16:02:57 +0300

    LogoutComponent

---
+++ reactmedicalstorefrontend/src/pages/LogoutComponent.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Navigate } from "react-router-dom"
+
+class LogoutComponent extends React.Component {
+    render() {
+        return < Navigate to="/" />
+    }
+}
